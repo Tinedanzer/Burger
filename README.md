456ed6c072474a2d3833f72bc7bb1e38f161cdc6
Tinedanzer <johnhillallen@gmail.com>
2018-10-21 01:57:17 -0500

    updates

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Burger
+
+This app is a full-stack persistent app using MySQL database and sequelize/express to work the communications to the front end. 
+
+There is also an implementation of handlebars ! Enjoy !!
--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 app=express();
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 const db = require("./models");
+// Static directory
+app.use(express.static("public"));
 // Sets up the Express app to handle data parsing
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
-// Static directory
-app.use(express.static("public"));
+// requiring handlebars and setting 'main'.handlebars as primary
+const exhbs = require("express-handlesbars");
+app.engine("handlebars", exhbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 // Routes
-// =============================================================
+require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
-require("./routes/author-api-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
-
 // Syncing our sequelize models and then starting our Express app
 // taking out db.sequelize.sync({force:true})
 db.sequelize.sync({}).then(function() {
